53e3ce63efadad96192b46003fc80e5c5f3d5e46
Michael Zhao <mzhaoau+1@gmail.com>
2021-08-04 19:01:06 +1000

    extract abstract repos

--- src/repos/in_memory/in_memory_player_repo.py
+++ src/repos/in_memory/in_memory_player_repo.py
@@ -2,10 +2,11 @@ from typing import List, Optional
 
 from src.entities.combo_set import ComboSet
 from src.entities.entry import Entry
+from src.repos.abstract.iplayer_repo import IPlayerRepo
 from src.repos.in_memory.data_store import DataStore
 
 
-class InMemoryPlayerRepo:
+class InMemoryPlayerRepo(IPlayerRepo):
     def __init__(self, data_store: DataStore):
         self.data_store = data_store
         self.next_entry_id = 0
