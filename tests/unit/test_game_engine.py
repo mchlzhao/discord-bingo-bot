80e1cee414f5aad9ceb6f1331ea2d513f9a4228a
Michael Zhao <mzhaoau+1@gmail.com>
2021-09-20 15:14:03 +1000

    finish event hit unit tests, still need to test bingo and views

--- tests/unit/test_game_engine.py
+++ tests/unit/test_game_engine.py
@@ -5,6 +5,7 @@ from unittest.mock import ANY, Mock
 
 from src.core.game_engine import GameEngine
 from src.core.game_engine_response import GameEngineResponse
+from src.entities.entry import Entry
 from src.entities.event import Event
 from src.entities.game import Game
 
@@ -22,6 +23,8 @@ class TestGameEngine(unittest.TestCase):
         self.fake_game = Game(0, self.fake_server_id,
                               datetime.now(), None)
         self.fake_event = Event(0, self.fake_game.game_id, 'event', 0)
+        self.fake_entry = Entry('entry', self.fake_game.game_id,
+                                self.fake_player_id, None)
 
     def assertResponseDisplayErrorContains(self, response: GameEngineResponse,
                                            display_error: str):
@@ -141,3 +144,135 @@ class TestGameEngine(unittest.TestCase):
             self.fake_game.game_id)
         self.player_repo.delete_entry.assert_not_called()
         self.player_repo.create_entry.assert_not_called()
+
+    def test_change_hit_hit_index(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseHasKeys(
+            self.engine.change_hit(self.fake_server_id, True, index=0),
+            ['event']
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_called_once_with(
+            self.fake_game.game_id, self.fake_event)
+
+    def test_change_hit_hit_desc(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseHasKeys(
+            self.engine.change_hit(self.fake_server_id, True, desc='event'),
+            ['event']
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_called_once_with(
+            self.fake_game.game_id, self.fake_event)
+
+    def test_change_hit_hit_desc_case_insensitive(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseHasKeys(
+            self.engine.change_hit(self.fake_server_id, True, desc='EVENT'),
+            ['event']
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_called_once_with(
+            self.fake_game.game_id, self.fake_event)
+
+    def test_change_hit_no_game(self):
+        self.game_repo.read_active_game.return_value = None
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, True, index=0),
+            'No game is currently running.'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.update_event.assert_not_called()
+
+    def test_change_hit_invalid_index(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, True, index=1),
+            'Invalid event index.'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_not_called()
+
+    def test_change_hit_no_match_desc(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, True, desc='event1'),
+            'No event description matches'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_not_called()
+
+    def test_change_hit_multiple_matches_desc(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = \
+            [self.fake_event, Event(1, self.fake_game.game_id, 'event2', 1)]
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, True, desc='event'),
+            'More than one event description matches'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_not_called()
+
+    def test_change_hit_hit_already_hit(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.fake_event.is_hit = True
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, True, index=0),
+            'already been hit'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_not_called()
+
+    def test_change_hit_unhit_already_unhit(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.event_repo.read_all_events.return_value = [self.fake_event]
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, False, index=0),
+            'already unhit'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_not_called()
+
+    def test_bingo(self):
+        self.game_repo.read_active_game.return_value = self.fake_game
+        self.player_repo.read_entry.return_value = self.fake_entry
+        self.assertResponseDisplayErrorContains(
+            self.engine.change_hit(self.fake_server_id, False, index=0),
+            'already unhit'
+        )
+        self.game_repo.read_active_game.assert_called_once_with(
+            self.fake_server_id)
+        self.event_repo.read_all_events.assert_called_once_with(
+            self.fake_game.game_id)
+        self.event_repo.update_event.assert_not_called()
